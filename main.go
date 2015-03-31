9d971f316262c7bc142bad69864ac5b586de63ad
datianshi <dsz0111@gmail.com>
2015-03-31 05:32:20 -0500

    Dig into the encryption

--- main.go
+++ main.go
@@ -1,36 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/base64"
+	"encoding/hex"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 
+	"github.com/datianshi/yaml"
 	"golang.org/x/crypto/pbkdf2"
-	"gopkg.in/yaml.v2"
 )
 
 type Installation struct {
-	Data string `yaml:":data"`
+	Data string `yaml:":data,binary"`
 	Salt string `yaml:":salt"`
-	Iv   string `yaml:":iv"`
+	Iv   string `yaml:":iv,binary"`
 	Md5  string `yaml:":md5"`
 }
 
 func main() {
-	text, err := decrypt()
-	if err != nil {
-		fmt.Println(err)
+	enc := flag.Bool("e", false, "encrypt")
+	dec := flag.Bool("d", false, "decrypt")
+	flag.Parse()
+	if *enc {
+		err := encrypt()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
-	fmt.Println(text)
+	if *dec {
+		text, err := decrypt()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(text)
+	}
 }
 
 func decrypt() (text string, err error) {
-	file, err := os.Open("installation.yml")
+	file, err := os.Open("encrypted.yml")
 	if err != nil {
 		return
 	}
@@ -46,7 +61,6 @@ func decrypt() (text string, err error) {
 	key := pbkdf2.Key([]byte("welcome"), []byte(ins.Salt), 4096, 32, sha1.New)
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		fmt.Println("error")
 		return
 	}
 	ivData, err := base64.StdEncoding.DecodeString(ins.Iv)
@@ -66,8 +80,58 @@ func decrypt() (text string, err error) {
 	return (string(unpadData)), nil
 }
 
+func encrypt() (err error) {
+	file, err := os.Open("installation.yml")
+	if err != nil {
+		return
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return
+	}
+	sum := md5.Sum(data)
+	salt := generateSalt()
+	key := pbkdf2.Key([]byte("welcome"), []byte(salt), 4096, 32, sha1.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	ivData := generateIV(block.BlockSize())
+	mode := cipher.NewCBCEncrypter(block, ivData)
+	paddedData := PKCS5Padding(data, block.BlockSize())
+	mode.CryptBlocks(paddedData, paddedData)
+	ins := Installation{
+		Data: string(paddedData),
+		Iv:   string(ivData),
+		Salt: salt,
+		Md5:  hex.EncodeToString(sum[:]),
+	}
+	result, err := yaml.Marshal(&ins)
+	if err != nil {
+		return
+	}
+	fmt.Printf(string(result))
+	return
+}
+func generateIV(size int) []byte {
+	b := make([]byte, size)
+	rand.Read(b)
+	return b
+}
+func generateSalt() string {
+	c := 10
+	b := make([]byte, c)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
+
 func PKCS5UnPadding(src []byte) []byte {
 	length := len(src)
 	unpadding := int(src[length-1])
 	return src[:(length - unpadding)]
+}
+func PKCS5Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
 }
